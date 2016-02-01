638d258af83208d03b6ebc30bb949335244276a9
Minh Son Nguyen <minh.son.nguyen.1209@gmail.com>
2016-02-01 17:05:50 +0200

    Basic structure and tasks

---
+++ README.md
@@ -0,0 +1,11 @@
+## My personal website
+*(Under development ...)*
+
+
+Author: Minh Son Nguyen
+
+Website: https://nguymin4.github.io
+
+E-mail: minh.son.nguyen.1209@gmail.com
+
+LinkedIn: https://www.linkedin.com/in/minhsonnguyen1209
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var path = require("path");
+
+module.exports = {
+	context: __dirname,
+	entry: {
+		site: "./src/js/site.js"
+	},
+	output: {
+		path: "./js",
+		filename: "[name].js"
+	},
+	cache: true,
+	module: {
+		loaders: [
+			{
+				test: /(\.js)|(\.jsx)$/,
+				include: [
+					path.resolve(__dirname, "src", "js"),
+					path.resolve(__dirname, "test", "unit")
+				],
+				// exclude: /(node_modules|bower_components|lib)/,
+				loader: "babel-loader",
+				query: {
+					presets: ['es2015'],
+					cacheDirectory: true
+				}
+			}
+		]
+	}
+}
