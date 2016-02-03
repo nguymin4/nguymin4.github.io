bb88c7b635b0019b6cce85938037338ce7727f52
Minh Son Nguyen <minh.son.nguyen.1209@gmail.com>
2016-02-03 02:54:12 +0200

    Add loading screen

--- js/site.js
+++ js/site.js
@@ -46,23 +46,33 @@
 
 	"use strict";
 
-	(function () {
-		var smoke = document.getElementsByClassName("smoke");
-		for (var i = 0; i < smoke.length; i++) {
-			smoke[i].style.left = 574 * (i - 1);
+	var documentLoaded = setInterval(function () {
+		if (/loaded|complete/.test(document.readyState)) {
+			clearInterval(documentLoaded);
+			document.body.className = "loaded";
+			init();
 		}
-		function floatSmoke() {
-			var l;
-			for (var i = 0; i < smoke.length; i++) {
-				l = parseInt(smoke[i].style.left);
-				if (l >= 574 * 4) l = -574;
-				smoke[i].style.left = l + 1;
-			}
-			requestAnimationFrame(floatSmoke);
-		}
+	}, 100);
 
-		// requestAnimationFrame(floatSmoke);
-	})();
+	function init() {
+		(function () {
+			var smoke = document.getElementsByClassName("smoke");
+			for (var i = 0; i < smoke.length; i++) {
+				smoke[i].style.left = 574 * i;
+			}
+			function floatSmoke() {
+				var l;
+				for (var i = 0; i < smoke.length; i++) {
+					l = parseInt(smoke[i].style.left);
+					if (l >= 574 * 4) l = -574;
+					smoke[i].style.left = l + 1;
+				}
+				requestAnimationFrame(floatSmoke);
+			}
+
+			// requestAnimationFrame(floatSmoke);
+		})();
+	}
 
 /***/ }
 /******/ ]);
--- src/js/site.js
+++ src/js/site.js
@@ -1,18 +1,27 @@
-(function () {
-	var smoke = document.getElementsByClassName("smoke");
-	for (var i = 0; i < smoke.length; i++) {
-		smoke[i].style.left = 574 * (i - 1);
+var documentLoaded = setInterval(function () {
+	if (/loaded|complete/.test(document.readyState)) {
+		clearInterval(documentLoaded);
+		document.body.className = "loaded"
+		init();
 	}
-	function floatSmoke() {
-		var l;
+}, 100);
+
+function init() {
+	(function () {
+		var smoke = document.getElementsByClassName("smoke");
 		for (var i = 0; i < smoke.length; i++) {
-			l = parseInt(smoke[i].style.left);
-			if (l >= 574 * 4) l = -574
-			smoke[i].style.left = l + 1
+			smoke[i].style.left = 574 * i;
 		}
-		requestAnimationFrame(floatSmoke)
-	}
+		function floatSmoke() {
+			var l;
+			for (var i = 0; i < smoke.length; i++) {
+				l = parseInt(smoke[i].style.left);
+				if (l >= 574 * 4) l = -574
+				smoke[i].style.left = l + 1
+			}
+			requestAnimationFrame(floatSmoke)
+		}
 
-	// requestAnimationFrame(floatSmoke);
-} ());
-
+		// requestAnimationFrame(floatSmoke);
+	})();
+}
