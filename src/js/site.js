dec705dfb034f1b4e0df672776a7cc42329cbfa0
Minh Son Nguyen <minh.son.nguyen.1209@gmail.com>
2016-02-07 10:39:01 +0200

    Restructure SCSS files and folders

--- src/js/site.js
+++ src/js/site.js
@@ -1,22 +1,24 @@
 var documentLoaded = setInterval(function () {
 	if (/loaded|complete/.test(document.readyState)) {
 		clearInterval(documentLoaded);
-		document.body.className = "loaded"
 		init();
+		document.body.className = "loaded"
 	}
 }, 100);
 
 function init() {
 	(function () {
 		var smoke = document.getElementsByClassName("smoke");
+		var style = window.getComputedStyle(smoke[0]);
+		var width = parseInt(style.width);
 		for (var i = 0; i < smoke.length; i++) {
-			smoke[i].style.left = 574 * i;
+			smoke[i].style.left = width * i;
 		}
 		function floatSmoke() {
 			var l;
 			for (var i = 0; i < smoke.length; i++) {
 				l = parseInt(smoke[i].style.left);
-				if (l >= 574 * 4) l = -574
+				if (l >= width * 4) l = -width
 				smoke[i].style.left = l + 1
 			}
 			requestAnimationFrame(floatSmoke)
