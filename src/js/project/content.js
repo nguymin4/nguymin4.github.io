aa7d32951f681321bbf84b059e88b07ed0303ce0
Minh Son Nguyen <minh.son.nguyen.1209@gmail.com>
2016-03-06 23:45:41 +0200

    Update Projects content

--- src/js/project/content.js
+++ src/js/project/content.js
@@ -1,5 +1,19 @@
 let eulerImg = "https://upload.wikimedia.org/wikipedia/commons/8/85/Leonhard_Euler_by_Darbes.jpg";
+let githubRoot = "https://github.com/nguymin4";
+
 export default[
+	{
+		title: "Personal website",
+		thumbnail: "assets/img/personal_website.jpg",
+		info: `<p><img align="right" src="assets/img/personal_website.jpg"  alt =" " width="300" />
+		This is project is my personal website (this website). Source code available on github.
+		About the front-end technologies I used:
+		<ul>
+			<li> HTML5
+			<li> CSS3 (SCSS)
+			<li> Javascript (ES6) packaged by webpack and transpiled by babel
+		</ul>`
+	},
 	{
 		title: "Project Euler - Python",
 		thumbnail: eulerImg,
@@ -14,11 +28,26 @@ export default[
 	{
 		title: "Lanban - Kanban web application",
 		thumbnail: "assets/img/lanban_board.jpg",
-		info: ``
+		info: `<h3>Kanban board web application for Lahti University of Applied Sciences (LAMK)</h3>
+<p>This web application development is a visual management tool which was developed as OSS and dedicated to LAMK. It can be used to control the flow of teamwork. 
+With the help of this tool, team is able to see who is working for which task, thus enable better visual communication over team’s overall work in a time box. Further the tool would be enabling better visibility to see how the work is progressing over time and illustrating received business value that is created in a project.</p></br>
+<p><strong>Value of the application:</strong> LAMK project teams can utilize agile methodology with lean approach. Electronic Kanban board
+would bring visibility to not only current work and team level but also to individual level: who is doing
+what and how much “points” have been earned within a sprint. Team leaders or “scrum masters”
+could see directly how efficiently the team is working and where to find the obstacles or barriers for
+working. The board also works as basis for every day standup meeting content (Show-and-tell)</p>
+<img class="img-responsive" src="https://github.com/nguymin4/Lanban/raw/master/Lanban/images/Chart-Demo.png"  alt=" " width="500" />
+<br />
+<img class="img-responsive" src="https://github.com/nguymin4/Lanban/raw/master/Lanban/images/Comment-Demo.png"  alt=" " width="500" />
+<br />
+<img class="img-responsive" src="https://github.com/nguymin4/Lanban/raw/master/Lanban/images/project.png"  alt=" " width="500" />
+`
 	},
 	{
 		title: "BookStore",
 		thumbnail: "https://raw.githubusercontent.com/nguymin4/aspnet-bookstore/master/docs/img/ss1.jpg",
-		info: `## BookStore`
+		info: `<h3>Book Store</h3>
+		<img src="https://github.com/nguymin4/aspnet-bookstore/raw/master/docs/img/ss3.jpg" alt=" " height="375" />
+		`
 	}
 ];
