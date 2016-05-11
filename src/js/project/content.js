b5995d30ff3a1c1b01bba2fc127ca31b451bd836
Minh Son Nguyen <minh.son.nguyen.1209@gmail.com>
2016-05-11 04:36:04 +0300

    Rename some js files

--- src/js/project/content.js
+++ src/js/project/content.js
@@ -1,17 +1,17 @@
-import react_sortable_demo from "./projects/react_sortable_demo.js";
-import personal_website from "./projects/personal_website.js";
-import project_euler_python from "./projects/project_euler_python.js";
+import react_sortable_demo from "./projects/react-sortable-demo.js";
+import personal_website from "./projects/personal-website.js";
+import project_euler_python from "./projects/project-euler-python.js";
 import bookstore from "./projects/bookstore.js";
 import lanban from "./projects/lanban.js";
-import productionsoftware_website from "./projects/productionsoftware_website.js";
+import productionsoftware_website from "./projects/productionsoftware-website.js";
 
 var getGithubLink = project => project.onGitHub ? `https://github.com/nguymin4/${project.id}` : "";
 
 export default [
 	react_sortable_demo,
 	personal_website,
-	project_euler_python,
 	bookstore,
+	project_euler_python,
 	lanban,
 	productionsoftware_website
 ].map(project => {
