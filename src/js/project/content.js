b3e22127df4d96fa802cebff577a1c732f9d5685
Minh Son Nguyen <minh.son.nguyen.1209@gmail.com>
2016-05-11 04:42:56 +0300

    Add project react-skype-ui

--- src/js/project/content.js
+++ src/js/project/content.js
@@ -1,3 +1,4 @@
+import react_skype_ui from "./projects/react-skype-ui.js";
 import react_sortable_demo from "./projects/react-sortable-demo.js";
 import personal_website from "./projects/personal-website.js";
 import project_euler_python from "./projects/project-euler-python.js";
@@ -8,6 +9,7 @@ import productionsoftware_website from "./projects/productionsoftware-website.js
 var getGithubLink = project => project.onGitHub ? `https://github.com/nguymin4/${project.id}` : "";
 
 export default [
+	react_skype_ui,
 	react_sortable_demo,
 	personal_website,
 	bookstore,
---
+++ src/js/project/projects/react-skype-ui.js
@@ -0,0 +1,12 @@
+export default {
+	id: "react-skype-ui",
+	title: "Mimic Skype Web UI using React",
+	thumbnail: "assets/img/react-skype-ui.png",
+	onGitHub: true,
+	info: `
+#### Mimic Skype Web UI using React
+*Still under development*
+<img align="right" src="assets/img/react-skype-ui.png"  alt =" " width="360" style="border: solid 1px #d4d4d4" />  
+Example: https://nguymin4.github.io/react-skype-ui  
+`
+};
