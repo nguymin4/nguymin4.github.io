3ba745f711eee00f53e5b7a3cc111079a4f904e2
Minh Son Nguyen <minh.son.nguyen.1209@gmail.com>
2020-03-15 22:04:50 +0200

    Show loader

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import Nav from '../components/Nav';
@@ -8,27 +8,43 @@ import About from '../views/About';
 import Skills from '../views/Skills';
 import '../styles/index.scss';
 
+const useLoader = () => {
+  const [isLoaded, hideLoader] = useState(false);
+  useEffect(() => {
+    setTimeout(() => hideLoader(true), 250);
+  }, []);
+  return isLoaded;
+};
+
 const App = ({ location: { hash } }) => {
+  const isLoaded = useLoader();
   const getClassName = (href) => (
     classnames('view', { active: href === hash })
   );
 
   return (
-    <div className="band">
-      <Smokes />
-      <Nav />
-      <div id="content">
-        <section id="home" className={getClassName('')}>
-          <Home />
-        </section>
-        <section id="about" className={getClassName('#about')}>
-          <About />
-        </section>
-        <section id="skills" className={getClassName('#skills')}>
-          <Skills />
-        </section>
+    <>
+      <div id="overlay" className={classnames({ loaded: isLoaded })}>
+        <div className="loader" />
       </div>
-    </div>
+      {isLoaded && (
+        <div className="band">
+          <Smokes />
+          <Nav />
+          <div id="content">
+            <section id="home" className={getClassName('')}>
+              <Home />
+            </section>
+            <section id="about" className={getClassName('#about')}>
+              <About />
+            </section>
+            <section id="skills" className={getClassName('#skills')}>
+              <Skills />
+            </section>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
