ac5f789bec4588aec4369d605192324c97b596e1
Minh Son Nguyen <minh.son.nguyen.1209@gmail.com>
2020-03-15 20:50:20 +0200

    Add 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Alert } from 'reactstrap';
+
+export default () => (
+  <div className="d-flex justify-content-center view active">
+    <Alert className="mt-5 p-5" color="light">
+      <h3>Page Not Found</h3>
+      <p>Oops, we could not find this page!</p>
+    </Alert>
+  </div>
+);
