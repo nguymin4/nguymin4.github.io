332486acdf789a08bd38ee339782e1b3f47ed890
Minh Son Nguyen <minh.son.nguyen.1209@gmail.com>
2020-03-18 01:10:59 +0200

    Set number of smokes based on screen size

--- src/components/Smokes.js
+++ src/components/Smokes.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import _ from 'lodash';
 
-const Smokes = () => (
-  <div id="smokes">
-    {_.range(4).map((i) => (
-      <div key={i} className="smoke" style={{ left: `${i * 574 * 2}px` }} />
-    ))}
-  </div>
-);
+const SMOKE_WIDTH = 574;
+
+const Smokes = () => {
+  const [numberOfSmokes, setNumberOfSmokes] = useState(0);
+  const update = _.debounce(() => {
+    const { innerWidth } = window;
+    const newNumberOfSmokes = Math.ceil(innerWidth / SMOKE_WIDTH) + 1;
+    setNumberOfSmokes(newNumberOfSmokes);
+  }, 200);
+
+  useEffect(() => {
+    update();
+    window.addEventListener('resize', update);
+    return () => window.removeEventListener('resize', update);
+  }, []);
+
+  return (
+    <div id="smokes">
+      {_.range(numberOfSmokes).map((i) => {
+        const left = `${i * SMOKE_WIDTH * 2}px`;
+        return (<div key={_.uniqueId()} className="smoke" style={{ left }} />);
+      })}
+    </div>
+  );
+};
 
 export default Smokes;
