b45e3cf76028867a3a8956e820a58791e7129fd0
Minh Son Nguyen <minh.son.nguyen.1209@gmail.com>
2020-06-12 17:33:11 +0300

    Updates

--- README.md
+++
@@ -1,5 +0,0 @@
-## My personal website
-This is the source code of my personal website.
-
-Author: Minh Son Nguyen  
-E-mail: minh.son.nguyen.1209@gmail.com
--- gatsby-node.js
+++
@@ -1,7 +0,0 @@
-exports.onCreatePage = ({ page, actions }) => {
-  if (page.path === '/') {
-    // eslint-disable-next-line
-    page.matchPath = '/';
-    actions.createPage(page);
-  }
-};
